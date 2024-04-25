173c2a763a32388e040c5b58ab0ae85f2c3537af
root <angelamarfo17@gmail.com>
2024-04-25 14:47:22 -0700

    new

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-'''The module provides function definition to calculate the minimum required
-operations to reach a given number of characters starting from a single
-character and only using "Copy All" and "Paste" operations'''
-from typing import Optional
+""" Module for 0-minoperations"""
 
 
-def minOperations(n: int, initialVal: int = 1, buf: int = 0) -> int:
-    '''The function calculate the minimum required operations to reach a given
-    number of characters starting from a single character and only using
-    "Copy All" and "Paste" operations'''
-    if n <= 1:
+def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-    elif buf == 0:
-        if n == 2:
-            return 2
-        return 2 + minOperations(n, 2 * initialVal, initialVal)
-
-    remainder = n - initialVal
-    if buf > remainder:
-        return -1
-    elif buf == remainder:
-        return 1
-    elif initialVal == remainder:
-        return 2
-
-    # Adding buffer (Just pasting without CopyAll)
-    ops_adding = minOperations(n, initialVal + buf, buf)  # if buf > 0 else -1
-    # Doubling: costs 2 operations (CopyAll and Paste)
-    ops_doubling = minOperations(n, 2 * initialVal, initialVal)
-
-    if ops_adding >= 0 and (ops_doubling < 0 or ops_adding <= ops_doubling):
-        return 1 + ops_adding
-    elif (ops_doubling >= 0):
-        return 2 + ops_doubling
-    return -1
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
